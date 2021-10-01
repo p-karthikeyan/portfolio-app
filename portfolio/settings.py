ac6ec25b20fa33b1f19035ff178c327dfda608cf
Karthik <82501176+p-karthikeyan@users.noreply.github.com>
2021-10-01 12:06:18 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='adhiyamaanstudent3@gmail.com'
-EMAIL_HOST_PASSWORD='innovation@2002'
+EMAIL_HOST_PASSWORD='**********'
 
 
 
